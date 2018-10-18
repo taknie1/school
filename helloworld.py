d949fe577ad8f5052eb60e1995ca21b503cfddf5
szbachulski <sz.bachulski@hotmail.com>
2018-10-18 21:30:24 +0200

    .....test

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 #Python 2.7 Samples
 
-import numpy as np
-print "Hello World"
-
-a = np.arange(15).reshape(3, 5)
-print(a)
+print "Hello World!"
+print("Hello World!")
+print(
+    "Hello World\n!"
+)
