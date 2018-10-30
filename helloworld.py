60c4910aac5a6869b9d453fd978955147fdb770b
szbachulski <sz.bachulski@hotmail.com>
2018-10-30 13:11:50 +0100

    .....test

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,42 @@
 #Python 2.7 Samples
 
+#1:
 print "Hello World!"
 print("Hello World!")
 print(
     "Hello World\n!"
 )
-print("Hello\t World!")
+print("Hello\t\t World!")
 
+#1.1
+print(3*"A to ciekawe! ")
+
+#1.2
+print("pi=%s" % "3.14159")
+
+#1.3
+
+#2: About comments:
+
+#Variables (zmienne):
+age = 28
+age2 = 28.
+print(age)
+print(type(age))
+print(type(age2))
+
+#Format (pyformat.info)
+
+print("{:2.2f}").format(age2)
+print("{:>10}").format(age2) #
+print("{:_>10}").format(age2) #Fill brackets
+print(".{:^10}.").format(age2) #Centered
+print(".{:^010}.").format(age2)
+
+
+
+hello_world = '"Hello World!"'
+hello_world2 = "'Hello World!'"
+print(hello_world+"\n"+hello_world2)
+print("\"%d")
 #Create a list:
