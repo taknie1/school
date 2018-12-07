8b92c3e02d9a6987137835d46005facefc0484dc
nervoxo <44392650+nervoxo@users.noreply.github.com>
2018-12-07 17:06:26 +0100

    Update nervoxo.py

--- 2018_2019/8Ca/Pierwszy_program/nervoxo.py
+++ 2018_2019/8Ca/Pierwszy_program/nervoxo.py
@@ -1,1 +1,13 @@
-
+A = input("Podaj dzielną:")
+A = int(A)
+B = input("Podaj dzielnik:")
+B = int(B)
+while B == 0:
+    print("Nie dziel przez 0")
+    B = input("Podaj dzielnik jeszcze raz:")
+    B = int(B)
+if B != 0:
+    print("Wynik:")
+    print(int(A/B))
+    print("Reszta:")
+    print(A%B)
