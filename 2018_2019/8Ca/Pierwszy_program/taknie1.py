af1f02bd6c9c133624939054d67b1d1edbde0cca
mikolajfras1 <44393039+taknie1@users.noreply.github.com>
2018-12-09 21:43:10 +0100

    zadanie domowe (to co na ocenę)
    Mikołaj Fraś 8c

--- 2018_2019/8Ca/Pierwszy_program/taknie1.py
+++ 2018_2019/8Ca/Pierwszy_program/taknie1.py
@@ -1,1 +1,10 @@
-
+#Program dzielacy dwie liczby przez siebie w wyniku podaje ilosc, i reszte z dzielenia
+a= float(input("a:"))
+b= float(input("b:"))
+while b==0:
+    print("!!!bug!!!")
+    b= float(input("b:"))
+if b!=0:
+    reszta=a%b
+    wynik=(a-reszta)/b
+    print("zniwo to:",wynik, "i",reszta, "odpadek to:",reszta)
